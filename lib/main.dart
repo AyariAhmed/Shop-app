2d0aaa9e11c86f32df202a6fabd79e69a65752fe
ayari17 <ayari17@hotmail.fr>
2021-03-14 18:13:08 +0100

    Added Launcher Icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
-
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/providers/products.dart';
