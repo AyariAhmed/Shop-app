e49316981c5086ef690ce736e3e86b3cf53f4d3c
ayari17 <ayari17@hotmail.fr>
2021-03-15 17:07:06 +0100

    Using FutureBuilder for initial fetching

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,63 +4,55 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
+
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    });
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    /*final orderData = Provider.of<Orders>(context);*/ // With FutureBuild this cause and infinite Loop
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders!'),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.add_shopping_cart),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/');
-              })
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: () async {
-                await Provider.of<Orders>(context, listen: false)
-                    .fetchAndSetOrders();
-              },
-              child: ListView.builder(
-                  itemCount: orderData.orders.length,
-                  itemBuilder: (ctx, index) {
-                    return OrderItem(orderData.orders[index]);
-                  }),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders!'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add_shopping_cart),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/');
+                })
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (dataSnapshot.error != null) {
+              // Error Handling code
+              return Center(
+                child: Text('An Error Occurred'),
+              );
+            } else {
+              return RefreshIndicator(
+                onRefresh: () async {
+                  await Provider.of<Orders>(context, listen: false)
+                      .fetchAndSetOrders();
+                },
+                child: Consumer<Orders>(
+                  builder: (ctx, Orders orderData, child) => ListView.builder(
+                      itemCount: orderData.orders.length,
+                      itemBuilder: (ctx, index) {
+                        return OrderItem(orderData.orders[index]);
+                      }),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
