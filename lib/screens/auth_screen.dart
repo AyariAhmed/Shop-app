076ede9077660ff2ad1f9f7729c6a893c29b74fe
AyariAhmed <ayari17@hotmail.fr>
2021-04-24 16:37:29 +0100

    Added Fade and slide Transitions

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -108,7 +108,8 @@ class _AuthCardState extends State<AuthCard>
   var containerHeight = 260;
 
   AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
 
   AuthMode _authMode = AuthMode.Login;
 
@@ -117,11 +118,13 @@ class _AuthCardState extends State<AuthCard>
     super.initState();
     _controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 350));
-    _heightAnimation = Tween<Size>(
-        begin: Size(double.infinity, 235), end: Size(double.infinity, 570))
+    _slideAnimation = Tween<Offset>(
+            begin: Offset(0,-1.5), end: Offset(0,0))
         .animate(
-        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
-   /* _heightAnimation.addListener(() {
+            CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    /* _heightAnimation.addListener(() {
       setState(() {});
     });*/
   }
@@ -152,8 +155,6 @@ class _AuthCardState extends State<AuthCard>
     _submitFocusNode.dispose();
     super.dispose();
   }
-
-
 
   Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
@@ -222,116 +223,123 @@ class _AuthCardState extends State<AuthCard>
       ),
       elevation: 8.0,
       child: AnimatedContainer(
-          duration: Duration(milliseconds: 350),
-          curve: Curves.fastOutSlowIn,
-          height: _authMode == AuthMode.Signup ? 570 : 245,
-          // height: _heightAnimation.value.height,
-          constraints:
-          BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 570 : 245,),
-          width: deviceSize.width * 0.78,
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'E-Mail'),
-                    keyboardType: TextInputType.emailAddress,
-                    onFieldSubmitted: (_) {
-                      FocusScope.of(context)
-                          .requestFocus(_passwordFocusNode);
-                    },
-                    validator: (value) {
-                      if (value.isEmpty ||
-                          !value.contains('@') ||
-                          !value.contains('.')) {
-                        return 'Invalid email!';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _authData['email'] = value;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    focusNode: _passwordFocusNode,
-                    onFieldSubmitted: (_) {
-                      FocusScope.of(context)
-                          .requestFocus(_confirmPasswordFocusNode);
-                    },
-                    controller: _passwordController,
-                    validator: (value) {
-                      if (value.isEmpty || value.length < 5) {
-                        return 'Password is too short!';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _authData['password'] = value;
-                    },
-                  ),
-                  if (_authMode == AuthMode.Signup)
-                    TextFormField(
-                      enabled: _authMode == AuthMode.Signup,
-                      decoration:
-                      InputDecoration(labelText: 'Confirm Password'),
-                      focusNode: _confirmPasswordFocusNode,
-                      onFieldSubmitted: (_) {
-                        FocusScope.of(context)
-                            .requestFocus(_submitFocusNode);
-                      },
-                      obscureText: true,
-                      validator: _authMode == AuthMode.Signup
-                          ? (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match!';
-                        }
-                        return null;
-                      }
-                          : null,
-                    ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  if (_isLoading)
-                    CircularProgressIndicator()
-                  else
-                  // ignore: deprecated_member_use
-                    RaisedButton(
-                      child: Text(_authMode == AuthMode.Login
-                          ? 'LOGIN'
-                          : 'SIGN UP'),
-                      onPressed: _submit,
-                      focusNode: _submitFocusNode,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+        duration: Duration(milliseconds: 350),
+        curve: Curves.fastOutSlowIn,
+        height: _authMode == AuthMode.Signup ? 570 : 245,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.Signup ? 570 : 245,
+        ),
+        width: deviceSize.width * 0.78,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_passwordFocusNode);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty ||
+                        !value.contains('@') ||
+                        !value.contains('.')) {
+                      return 'Invalid email!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['email'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  focusNode: _passwordFocusNode,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context)
+                        .requestFocus(_confirmPasswordFocusNode);
+                  },
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return 'Password is too short!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value;
+                  },
+                ),
+
+                AnimatedContainer(
+                  curve: Curves.easeIn,
+                  constraints: BoxConstraints(
+                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0),
+                  duration: Duration(milliseconds: 300),
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        focusNode: _confirmPasswordFocusNode,
+                        onFieldSubmitted: (_) {
+                          FocusScope.of(context).requestFocus(_submitFocusNode);
+                        },
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
                       ),
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 30.0, vertical: 8.0),
-                      color: Theme.of(context).primaryColor,
-                      textColor:
-                      Theme.of(context).primaryTextTheme.button.color,
                     ),
-                  // ignore: deprecated_member_use
-                  FlatButton(
-                    child: Text(
-                        '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                    onPressed: _switchAuthMode,
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 30.0, vertical: 4),
-                    materialTapTargetSize:
-                    MaterialTapTargetSize.shrinkWrap,
-                    textColor: Theme.of(context).primaryColor,
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  // ignore: deprecated_member_use
+                  RaisedButton(
+                    child:
+                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                    onPressed: _submit,
+                    focusNode: _submitFocusNode,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                  ),
+                // ignore: deprecated_member_use
+                FlatButton(
+                  child: Text(
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                  onPressed: _switchAuthMode,
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Theme.of(context).primaryColor,
+                ),
+              ],
             ),
           ),
         ),
-
+      ),
     );
   }
 }
