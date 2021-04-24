4ac0b9b2bde3f8d0f67e9fa8cc618580297e8db5
AyariAhmed <ayari17@hotmail.fr>
2021-04-24 21:44:22 +0100

    custom transition for all routes

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -16,3 +16,17 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
     );
   }
 }
+
+class CustomPageTransitionBuilder extends PageTransitionsBuilder{
+  @override
+  Widget buildTransitions<T>(PageRoute<T> route,BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (route.settings.name == '/') {
+      return child;
+    }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
@@ -40,6 +41,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.grey,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android : CustomPageTransitionBuilder(),
+              TargetPlatform.iOS : CustomPageTransitionBuilder(),
+            })
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
